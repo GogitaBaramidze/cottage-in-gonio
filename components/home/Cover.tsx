7762fe14ef44bf2c4f7a80ed50a9d8d3ed1b56d8
Lado Asambadze <lado.asambadze1@gmail.com>
2024-05-04 16:27:53 +0400

    add mobile cover img

--- components/home/Cover.tsx
+++ components/home/Cover.tsx
@@ -1,19 +1,24 @@
 'use client'
 
 import Image from 'next/image'
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
 import MainBg from '../../public/images/mainBg.jpg'
+import MobileBg from '../../public/images/wine/BottleWithGlasses.jpg'
 
 export default function Cover() {
-    const { t } = useTranslation()
+    // const { t } = useTranslation()
 
     return (
         <section className="flex h-auto w-full flex-col items-center   text-white  shadow-xl  ">
-            <Image src={MainBg} className="h-full w-full object-cover" alt="cover" />
-
-            <span className="absolute mt-24 font-moderline  text-4xl tracking-wider md:mt-[400px] md:text-[72px]">
+            <Image
+                src={MainBg}
+                className="hidden h-full w-full object-cover md:block"
+                alt="cover"
+            />
+            <Image src={MobileBg} className="h-full w-full object-cover md:hidden" alt="cover" />
+            {/* <span className="absolute mt-24 font-moderline  text-4xl tracking-wider md:mt-[400px] md:text-[72px]">
                 {t('chateau')}
-            </span>
+            </span> */}
         </section>
     )
 }
