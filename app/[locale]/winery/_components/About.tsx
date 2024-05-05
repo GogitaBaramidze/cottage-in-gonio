1874b1b20dac1eab895992c4cbb334d090ef9ed0
Lado Asambadze <lado.asambadze1@gmail.com>
2024-05-05 23:46:39 +0400

    modift list componenet

--- app/[locale]/winery/_components/About.tsx
+++ app/[locale]/winery/_components/About.tsx
@@ -1,17 +1,15 @@
 export default function About() {
     return (
-        <section className="flex md:px-7 px-5 h-auto w-full flex-col items-center bg-[#f1efeb] pb-20 pt-10">
+        <section className="flex h-auto w-full flex-col items-center bg-[#f1efeb] px-5 pb-20 pt-10 md:px-7">
             <h1 className=" font-canela text-3xl md:text-5xl ">
                 Experience Georiga <br /> Winemaking at
-                <span className="ml-5 leading-normal font-moderline">its best</span>
+                <span className="ml-5 font-moderline leading-normal">its best</span>
             </h1>
-            <p className="mt-5 px-5  xl:px-48 text- md:text-base text-center">
+            <p className="text- mt-5  px-5 text-center md:text-base xl:px-48">
                 From humble beginnings, Chateau Elan Winery has emerged as one of the largest and
                 most awarded wineries on the East Coast. Were honored to offer more than 30 wines
                 produced with grapes grown in our Braselton and south of Georgia vineyards, as well
-                as grapes hand-picked and transported, whole cluster, in refrigerated trucks from
-                Californias Lake County and Clarksburg regions. Our North Georgia winery is open
-                daily for guided tours and tastings, as well as one-of-a-kind private events.
+                as grapes hand-picked and transported
             </p>
         </section>
     )
