188ef55434382400b96c08e19976c6b3771446a1
Lado Asambadze <lado.asambadze1@gmail.com>
2024-05-10 16:49:04 +0400

    Add some items and modify styles on about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 import { useState } from 'react'
 import CountUp from 'react-countup'
 import Image from 'next/image'
-import Building from '../../../public/images/Building.jpg'
+import Family from '../../../public/images/about/Family.jpg'
 import { Visitors } from '@/components/svgs'
 
 const textAnimation = {
@@ -22,116 +22,108 @@ const textAnimation = {
     }),
 }
 
-const imageAnimation = {
-    hidden: {
-        y: 100,
-        opacity: 0,
-        scale: 0.9,
-    },
-    visible: (custom: string | any) => ({
-        y: 0,
-        opacity: 1,
-        scale: 1,
-        transition: { delay: custom * 0.1 },
-    }),
-}
 const ScrollTriggerComponent = ScrollTrigger as React.ComponentType<any>
 export default function About() {
     const [count, setCount] = useState(false)
     return (
         <>
             <ScrollTriggerComponent onEnter={() => setCount(true)} onExit={() => setCount(false)}>
-                <motion.section
-                    viewport={{ amount: 0.2 }}
-                    initial="hidden"
-                    whileInView="visible"
-                    className="flex min-h-screen w-full flex-row items-center gap-10 px-20 py-10"
-                >
-                    <div className="flex w-[600px] flex-col">
+                <div className="relative flex h-auto w-full flex-row gap-20 py-10 pl-24">
+                    <div className="flex h-[600px] w-full flex-col items-center gap-5">
+                        <h1>qeglqmegqegqeg</h1>
                         <p>
-                            qegkpqeigjqgjqeipgqeipgqeigqeipjgqipejgqijgqejpjipgeqijqgeijpgqjiqgejiqegjpiqjpiqjpg
+                            kqefmjqeimgfqeim gik qemgqe mgmqegqegmqegeq kqefmjqeimgfqeim gik qemgqe
+                            mgmqegqegmqegeqkqefmjqeimgfqeim gik qemgqe
+                            mgmqegqegmqegeqkqefmjqeimgfqeim gik qemgqe mgmqegqegmqegeq
                         </p>
-                        <div className="grid h-full w-full grid-cols-2 grid-rows-2  gap-5  ">
-                            <motion.div
-                                custom={1}
-                                variants={textAnimation}
-                                className=" flex w-full flex-row items-center rounded-md bg-zinc-800 p-2"
-                            >
-                                <Visitors />
-                                {count && (
-                                    <CountUp
-                                        className="ml-1 text-white"
-                                        start={1}
-                                        end={20000}
-                                        duration={3}
-                                    />
-                                )}
-                                <span className="ml-1 text-white">+ visitors</span>
-                            </motion.div>
-                            <motion.div
-                                custom={2}
-                                variants={textAnimation}
-                                className="flex w-full flex-row items-center rounded-md bg-zinc-800 p-2"
-                            >
-                                <Visitors />
-                                {count && (
-                                    <CountUp
-                                        className="ml-1 text-white"
-                                        start={1}
-                                        end={19}
-                                        duration={3}
-                                    />
-                                )}
-                                <span className="ml-1 text-white">+ weddings</span>
-                            </motion.div>
-                            <motion.div
-                                custom={3}
-                                variants={textAnimation}
-                                className=" flex  w-full  flex-row items-center rounded-md bg-zinc-800 p-2"
-                            >
-                                <Visitors />
-                                {count && (
-                                    <CountUp
-                                        className="ml-1 text-white"
-                                        start={1}
-                                        end={30}
-                                        duration={3}
-                                    />
-                                )}
-                                <span className="ml-1 text-white">+ meeting</span>
-                            </motion.div>
-                            <motion.div
-                                custom={4}
-                                variants={textAnimation}
-                                className="flex w-full flex-row items-center rounded-md bg-zinc-800 p-2"
-                            >
-                                <Visitors />
-                                {count && (
-                                    <CountUp
-                                        className="ml-1 text-white"
-                                        start={1}
-                                        end={15}
-                                        duration={3}
-                                    />
-                                )}
-                                <span className="ml-1 text-white">+ event</span>
-                            </motion.div>
-                        </div>
+                        <p>
+                            kqefmjqeimgfqeim gik qemgqe mgmqegqegmqegeq kqefmjqeimgfqeim gik qemgqe
+                            mgmqegqegmqegeqkqefmjqeimgfqeim gik qemgqe
+                            mgmqegqegmqegeqkqefmjqeimgfqeim gik qemgqe mgmqegqegmqegeq
+                        </p>
+                        <p>
+                            kqefmjqeimgfqeim gik qemgqe mgmqegqegmqegeq kqefmjqeimgfqeim gik qemgqe
+                            mgmqegqegmqegeqkqefmjqeimgfqeim gik qemgqe
+                            mgmqegqegmqegeqkqefmjqeimgfqeim gik qemgqe mgmqegqegmqegeq
+                        </p>
+                        <motion.section
+                            viewport={{ amount: 0.2 }}
+                            initial="hidden"
+                            whileInView="visible"
+                            className="flex h-auto  w-full  flex-col items-center gap-5  "
+                        >
+                            <div className="flex w-full flex-col">
+                                <div className="grid h-auto w-full grid-cols-2 grid-rows-2  gap-5  ">
+                                    <motion.div
+                                        custom={1}
+                                        variants={textAnimation}
+                                        className=" flex w-full flex-row items-center rounded-md bg-zinc-800 p-2"
+                                    >
+                                        <Visitors />
+                                        {count && (
+                                            <CountUp
+                                                className="ml-1 text-white"
+                                                start={1}
+                                                end={20000}
+                                                duration={3}
+                                            />
+                                        )}
+                                        <span className="ml-1 text-white">+ visitors</span>
+                                    </motion.div>
+                                    <motion.div
+                                        custom={2}
+                                        variants={textAnimation}
+                                        className="flex w-full flex-row items-center rounded-md bg-zinc-800 p-2"
+                                    >
+                                        <Visitors />
+                                        {count && (
+                                            <CountUp
+                                                className="ml-1 text-white"
+                                                start={1}
+                                                end={19}
+                                                duration={3}
+                                            />
+                                        )}
+                                        <span className="ml-1 text-white">+ weddings</span>
+                                    </motion.div>
+                                    <motion.div
+                                        custom={3}
+                                        variants={textAnimation}
+                                        className=" flex  w-full  flex-row items-center rounded-md bg-zinc-800 p-2"
+                                    >
+                                        <Visitors />
+                                        {count && (
+                                            <CountUp
+                                                className="ml-1 text-white"
+                                                start={1}
+                                                end={30}
+                                                duration={3}
+                                            />
+                                        )}
+                                        <span className="ml-1 text-white">+ meeting</span>
+                                    </motion.div>
+                                    <motion.div
+                                        custom={4}
+                                        variants={textAnimation}
+                                        className="flex w-full flex-row items-center rounded-md bg-zinc-800 p-2"
+                                    >
+                                        <Visitors />
+                                        {count && (
+                                            <CountUp
+                                                className="ml-1 text-white"
+                                                start={1}
+                                                end={15}
+                                                duration={3}
+                                            />
+                                        )}
+                                        <span className="ml-1 text-white">+ event</span>
+                                    </motion.div>
+                                </div>
+                            </div>
+                        </motion.section>
                     </div>
-                    <motion.div variants={imageAnimation} custom={1} className="">
-                        <Image src={Building} className="h-full max-w-full" alt="building" />
-                    </motion.div>
-
-                    <motion.div variants={imageAnimation} custom={2}>
-                        <Image src={Building} className="h-full max-w-full" alt="building" />
-                    </motion.div>
-                    <motion.div variants={imageAnimation} custom={3}>
-                        <Image src={Building} className="h-full max-w-full" alt="building" />
-                    </motion.div>
-                    <motion.div variants={imageAnimation} custom={4}>
-                        <Image src={Building} className="h-full max-w-full" alt="building" />
-                    </motion.div>
-                </motion.section>
+                    <Image src={Family} className="h-full w-1/2 object-cover" alt="building" />
+                </div>
             </ScrollTriggerComponent>
         </>
     )
--- app/[locale]/contact/_components/Header.tsx
+++ app/[locale]/contact/_components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <section className="flex h-auto w-full flex-col items-center gap-6 md:gap-8 px-5">
+        <section className="flex h-auto w-full flex-col items-center gap-10 px-5">
             <h1 className=" font-moderline text-4xl lg:text-5xl font-semibold">Chateau Iveri</h1>
             <div className="grid grid-cols-1 md:grid-cols-2   gap-x-4 md:gap-y-4 gap-y-3 text-sm  ">
                 <span>
